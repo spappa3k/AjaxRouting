ccb248543d441e2543437c7ba436e5718e7576e5
spappa3k <pappalardo.dev@gmail.com>
2024-06-29 12:35:47 +0200

    .

---
+++ src/app/albums-list/albums-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-list',
+  templateUrl: './albums-list.component.html',
+  styleUrl: './albums-list.component.css'
+})
+export class AlbumsListComponent {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AlbumsListComponent } from './albums-list/albums-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', 
+    component:HomeComponent
+  },
+  {
+    path: 'albums', 
+    component:AlbumsListComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+ }
